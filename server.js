3e6dde15cfb77499586c1130dbdbdc6c68abb5f9
Richard Hunter <richard@richardhunter.co.uk>
2016-09-16 00:24:13 +0100

    reset css

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ port = process.env.PORT || 5000;
 app.use('/node_modules', express.static('./node_modules'));
 app.use('/app', express.static('./app'));
 app.use('/config', express.static('./config'));
+app.use('/styles', express.static('./styles'));
 
 app.set('view engine', 'ejs');
 
